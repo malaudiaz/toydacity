16fbd24cb90022fe0207450e85ebee5f265c928d
malaudiaz <113136614+malaudiaz@users.noreply.github.com>
2025-04-23 13:54:41 -0400

    Modificado el config

--- app/core/config.py
+++ app/core/config.py
@@ -1,24 +1,25 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
 from dotenv import load_dotenv
+import os
 
 load_dotenv()  # Carga el .env antes que Pydantic
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Toydacity"
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = Field(default="S3cr3t_K3y")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(default=30)
-    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+    REFRESH_TOKEN_EXPIRE_DAYS: int = os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7)
     
     # Google OAuth
-    GOOGLE_CLIENT_ID: str = Field(default="925708649710-pjq0tnkq6qrrqjm6dqprj5moja1oa4v0.apps.googleusercontent.com")
-    GOOGLE_CLIENT_SECRET: str = Field(default="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")
-    GOOGLE_CLIENT_REDIRECT_URI: str = Field(default="http://127.0.0.1:8000/api/v1/google/callback")
+    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID", "")
+    GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET", "")
+    GOOGLE_CLIENT_REDIRECT_URI: str = os.getenv("GOOGLE_CLIENT_REDIRECT_URI", "")
        
     # Database
-    DATABASE_URL: str = "postgresql://admin:pgsql09@localhost:5432/toydacity"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://usr:password@localhost:5432/toydacity")
           
     class Config:
         env_file = ".env" 
